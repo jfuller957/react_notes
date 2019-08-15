b10459d7e653fb7df297701dd1801d61c1b7b9c2
James Fuller <jfuller957@gmail.com>
2019-08-14 18:14:28 -0700

    generate numbers and waiting status with .5 sec timeOut

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ class App extends Component {
   constructor(){
     super();
     this.state= {
-      numbers: [
-        1, 2, 3, 9
-      ]
+      numbers: [],
+      waiting: true
     };
   }
+  componentDidMount(){
+    setTimeout(()=> {
+      this.setState({
+        numbers: [
+          1, 2, 3, 9
+        ],
+        waiting: false
+      });
+    }, 500);
+  }
   render(){
-    const { numbers } = this.state;
+    const { numbers, waiting } = this.state;
+    if(waiting){
+      return React.createElement('div', null, '...wait for it...');
+    }
     const lis = numbers.map( (number, idx) => React.createElement('li', { key: idx}, number)
     );
     return React.createElement('ul', null, lis);
